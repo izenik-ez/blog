e28bcd3bbd278f8a66ddde155612859d1032c0db
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2023-12-16 18:26:51 +0100

    End blog example

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+  
+  has_many :comments, dependent: :destroy
+  
   # Validation are rules that are checked before a model object is saved.
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
