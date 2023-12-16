96f591eea0ce48ca6a0e9a9a31475df9ad8d53e1
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2023-12-16 13:41:10 +0100

    End Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  # Validation are rules that are checked before a model object is saved.
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
